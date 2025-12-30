17a5f014ed5751c7fde8f89c2c8ccc90896d25d6
Lorenzo Paolo Cocchinone <lorenzopaolo.cocchinone@kineton.it>
2025-12-30 17:01:36 +0100

    chore: add preparing status

--- src/constants/PipelineStatus.ts
+++ src/constants/PipelineStatus.ts
@@ -1,8 +1,8 @@
 enum BuildStatus {
   Requested = "Requested",     // Build requested from CMS, EC2 Spot instance not yet scheduled/launched.
   Pending = "Pending",         // Build waiting for an EC2 SPOT to be assigned.
-  Init = "Init",               // Repository and environment setup in progress.
   Started = "Started",         // EC2 Spot instance is running and the build process has begun.
+  Preparing = "Preparing",     // Create and update files for start the build
   Building = "Building",       // App compilation in progress.
   Signing = "Signing",         // APK/AAB is being digitally signed.
   Publishing = "Publishing",   // Uploading the build to the store.
